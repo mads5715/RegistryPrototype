49b4ea6102e43be3ce566c653774d8641cb555e0
madsdurrwium <madsdurrwium@gmail.com>
2019-10-06 23:09:26 +0200

    Gotten upload more or less working

---
+++ BE/UploadObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistryPrototype.BE
+{
+    public class UploadObject
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string readme { get; set; }
+
+    }
+    
+}
--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SystemFile = System.IO.File;
 
 namespace RegistryPrototype.Controllers
 {
@@ -16,15 +19,29 @@ namespace RegistryPrototype.Controllers
         public IActionResult Get(string name)
         {
             Debug.WriteLine("NPM want's package with name: "+ name);
-            return Ok("{ \"versions\":{ \"0.0.0\":{ \"name\":\"helloworld\",\"version\":\"0.0.0\",\"dependencies\":{ },\"devDependencies\":{ },\"_hasShrinkwrap\":false,\"directories\":{ },\"dist\":{ \"shasum\":\"2e5cc78b0fe5708bde8410e0f4dd2b9e328dd357\",\"tarball\":\"https://registry.npmjs.org/helloworld/-/helloworld-0.0.0.tgz\"},\"engines\":{ \"node\":\"*\"} } },\"name\":\"helloworld\",\"dist-tags\":{ \"latest\":\"0.0.0\"},\"modified\":\"2011-09-20T23:58:58.133Z\"}");
+            var headers = HttpContext.Request.Headers;
+            foreach (var item in headers)
+            {
+                Console.WriteLine(item.Key + " / " + item.Value);
+            }
+            using (var reader = new StreamReader(HttpContext.Request.Body))
+            {
+                var body = reader.ReadToEnd();
+
+                Console.WriteLine("Body: " + body);
+            }
+
+            
+            return Ok("{ \"versions\":{ \"0.0.0\":{ \"name\":\"helloworld\",\"version\":\"0.0.0\",\"dependencies\":{ },\"devDependencies\":{ },\"_hasShrinkwrap\":false,\"directories\":{ },\"dist\":{ \"shasum\":\"2e5cc78b0fe5708bde8410e0f4dd2b9e328dd357\",\"tarball\":\"http://192.168.0.10/helloworld/-/helloworld-0.0.0.tgz\"},\"engines\":{ \"node\":\"*\"} } },\"name\":\"helloworld\",\"dist-tags\":{ \"latest\":\"0.0.0\"},\"modified\":\"2011-09-20T23:58:58.133Z\"}");
         }
 
         // GET api/values/5
         [HttpGet]
-        [Route("./")]
-        public ActionResult<string> Get(int id)
+        [Route("{packagename}/-/{filename}")]
+        public IActionResult GetFile(string packagename, string filename)
         {
-            return "value";
+            Console.WriteLine("NPM requested the following: "+packagename+"/-/"+filename);
+            return Ok();
         }
 
         // POST api/values
@@ -34,9 +51,35 @@ namespace RegistryPrototype.Controllers
         }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{name}")]
+        public IActionResult Put(string name)
         {
+            Debug.WriteLine("NPM want's to publish package with name: " + name);
+            var headers = HttpContext.Request.Headers;
+            foreach (var item in headers)
+            {
+                Console.WriteLine(item.Key + " / " + item.Value);
+            }
+            var data = new byte[10240];//10meg buffer...
+            var filename = "";
+            var regex = "^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$";
+            using (var reader = new StreamReader(HttpContext.Request.Body))
+            {
+                var body = reader.ReadToEnd();
+                var jsonObj = JObject.Parse(body);
+                var attachmentlenth = jsonObj["_attachments"]["testpackage-1.0.0.tgz"]["length"];
+                filename = jsonObj["name"].ToString()+ "-" + jsonObj["dist-tags"]["latest"].ToString()+".tgz";
+                data = Convert.FromBase64String(jsonObj["_attachments"][filename]["data"].ToString());
+                Console.WriteLine("Upload length: "+ attachmentlenth);
+                if (Convert.ToInt32(attachmentlenth) == data.Length)
+                {
+                    Console.WriteLine("The length matched so we can be sure it's at least somewhat not corrupted!");
+                }
+                Console.WriteLine("Filename: "+ filename);
+                Console.WriteLine("Body: " + body);
+            }
+            SystemFile.WriteAllBytes(filename, data);
+            return Ok();
         }
 
         // DELETE api/values/5

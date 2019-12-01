20a23f22bb5dd34093d7e0c02a2f3148fbab094c
madsdurrwium <madsdurrwium@gmail.com>
2019-12-01 14:50:36 +0100

    Updated SQL creation script to reflect latest updates.

--- mysql-scripts/CreateAllDBs.sql
+++ mysql-scripts/CreateAllDBs.sql
@@ -10,174 +10,7 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*
-    Copyright (C) 2019  Mads Dürr-Wium
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
--- Dumping database structure for HangfireRegistry
-CREATE DATABASE IF NOT EXISTS `HangfireRegistry` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
-USE `HangfireRegistry`;
-
--- Dumping structure for table HangfireRegistry.HangFire_AggregatedCounter
-CREATE TABLE IF NOT EXISTS `HangFire_AggregatedCounter` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Key` varchar(100) NOT NULL,
-  `Value` int(11) NOT NULL,
-  `ExpireAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `IX_CounterAggregated_Key` (`Key`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_Counter
-CREATE TABLE IF NOT EXISTS `HangFire_Counter` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Key` varchar(100) NOT NULL,
-  `Value` int(11) NOT NULL,
-  `ExpireAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `IX_Counter_Key` (`Key`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_DistributedLock
-CREATE TABLE IF NOT EXISTS `HangFire_DistributedLock` (
-  `Resource` varchar(100) NOT NULL,
-  `CreatedAt` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_Hash
-CREATE TABLE IF NOT EXISTS `HangFire_Hash` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Key` varchar(100) NOT NULL,
-  `Field` varchar(40) NOT NULL,
-  `Value` longtext,
-  `ExpireAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `IX_Hash_Key_Field` (`Key`,`Field`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_Job
-CREATE TABLE IF NOT EXISTS `HangFire_Job` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `StateId` int(11) DEFAULT NULL,
-  `StateName` varchar(20) DEFAULT NULL,
-  `InvocationData` longtext NOT NULL,
-  `Arguments` longtext NOT NULL,
-  `CreatedAt` datetime NOT NULL,
-  `ExpireAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `IX_Job_StateName` (`StateName`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_JobParameter
-CREATE TABLE IF NOT EXISTS `HangFire_JobParameter` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `JobId` int(11) NOT NULL,
-  `Name` varchar(40) NOT NULL,
-  `Value` longtext,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `IX_JobParameter_JobId_Name` (`JobId`,`Name`),
-  KEY `FK_JobParameter_Job` (`JobId`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_JobQueue
-CREATE TABLE IF NOT EXISTS `HangFire_JobQueue` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `JobId` int(11) NOT NULL,
-  `Queue` varchar(50) NOT NULL,
-  `FetchedAt` datetime DEFAULT NULL,
-  `FetchToken` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  KEY `IX_JobQueue_QueueAndFetchedAt` (`Queue`,`FetchedAt`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_JobState
-CREATE TABLE IF NOT EXISTS `HangFire_JobState` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `JobId` int(11) NOT NULL,
-  `Name` varchar(20) NOT NULL,
-  `Reason` varchar(100) DEFAULT NULL,
-  `CreatedAt` datetime NOT NULL,
-  `Data` longtext,
-  PRIMARY KEY (`Id`),
-  KEY `FK_JobState_Job` (`JobId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_List
-CREATE TABLE IF NOT EXISTS `HangFire_List` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Key` varchar(100) NOT NULL,
-  `Value` longtext,
-  `ExpireAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_Server
-CREATE TABLE IF NOT EXISTS `HangFire_Server` (
-  `Id` varchar(100) NOT NULL,
-  `Data` longtext NOT NULL,
-  `LastHeartbeat` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_Set
-CREATE TABLE IF NOT EXISTS `HangFire_Set` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Key` varchar(100) NOT NULL,
-  `Value` varchar(256) NOT NULL,
-  `Score` float NOT NULL,
-  `ExpireAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `IX_Set_Key_Value` (`Key`,`Value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
--- Dumping structure for table HangfireRegistry.HangFire_State
-CREATE TABLE IF NOT EXISTS `HangFire_State` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `JobId` int(11) NOT NULL,
-  `Name` varchar(20) NOT NULL,
-  `Reason` varchar(100) DEFAULT NULL,
-  `CreatedAt` datetime NOT NULL,
-  `Data` longtext,
-  PRIMARY KEY (`Id`),
-  KEY `FK_HangFire_State_Job` (`JobId`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
 
 -- Dumping database structure for NPMRegistryClone
 CREATE DATABASE IF NOT EXISTS `NPMRegistryClone` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
@@ -189,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `OrgDepartments` (
   `Name` char(250) COLLATE utf8mb4_bin NOT NULL DEFAULT 'Company',
   `OrgDesc` text COLLATE utf8mb4_bin,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- Data exporting was unselected.
 
@@ -211,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `Packages` (
   `CanBeServedFromServer` bit(1) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `_ID` (`_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1302 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=1625 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- Data exporting was unselected.
 
@@ -233,13 +66,27 @@ CREATE TABLE IF NOT EXISTS `UserPackages` (
 CREATE TABLE IF NOT EXISTS `Users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `UserName` char(250) COLLATE utf8mb4_bin DEFAULT 'Username',
+  `UserPassword` char(255) COLLATE utf8mb4_bin DEFAULT 'Password',
+  `UserType` int(11) DEFAULT NULL,
   `DisplayName` char(250) COLLATE utf8mb4_bin DEFAULT 'Username',
   `OrgDepartment` int(10) DEFAULT NULL,
+  `Email` varchar(250) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `DisplayName` (`DisplayName`),
   KEY `FK_Users_OrgDepartments` (`OrgDepartment`),
-  CONSTRAINT `FK_Users_OrgDepartments` FOREIGN KEY (`OrgDepartment`) REFERENCES `OrgDepartments` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  KEY `FK_Users_UserTypes` (`UserType`),
+  CONSTRAINT `FK_Users_OrgDepartments` FOREIGN KEY (`OrgDepartment`) REFERENCES `OrgDepartments` (`ID`),
+  CONSTRAINT `FK_Users_UserTypes` FOREIGN KEY (`UserType`) REFERENCES `UserTypes` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table NPMRegistryClone.UserTypes
+CREATE TABLE IF NOT EXISTS `UserTypes` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(250) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- Data exporting was unselected.
 
